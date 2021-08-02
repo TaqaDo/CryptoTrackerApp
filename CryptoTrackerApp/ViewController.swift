8dcd317d76a176f75a3d1ac6d5ee8ac3a02c1f10
TaqaDo <osmonov1337@gmail.com>
2021-08-02 23:57:13 +0600

    Initial Commit

---
+++ CryptoTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTrackerApp
+//
+//  Created by talgar osmonov on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
